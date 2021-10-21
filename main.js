cfd74e33773e62df0f02bd31879aabc7aace1206
Mike Echevarria <mike.echevarria@gmail.com>
2021-10-21 16:08:06 -0400

    full parsing to json done

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const stream = fs.createReadStream('Inbox.mbox');
 const cliProgress = require('cli-progress');
 const mbox = new Mbox(stream);
+const simpleParser = require('mailparser').simpleParser;
 
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
@@ -10,16 +11,26 @@ const messages = [];
 
 console.info('Loading mbox file');
 mbox.on('message', (msg) => {
-  messages.push(msg.toString);
+  messages.push(msg);
 });
 
 mbox.on('error', (err) => {
-  console.error('got an error', err);
+  console.error('error in mbox loading', err);
 });
 
 mbox.on('end', () => {
   console.info('\nDone parsing mbox');
   console.info(`${messages.length} emails in mbox file`);
+
+  const rawMsg = messages[0];
+  const parseOptions = {};
+  simpleParser(rawMsg, parseOptions)
+    .then((parsed) => {
+      console.info(parsed);
+    })
+    .catch((err) => {
+      console.error('parsing error', err);
+    });
 
   // bar.start(messages.length, 0);
 
