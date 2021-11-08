f1c97765a6da2b0e62c9e8bc3f4f729680afd34c
Mike Echevarria <mike.echevarria@gmail.com>
2021-11-08 16:57:06 -0500

    add columns and create insert statement

--- hana-client.js
+++ hana-client.js
@@ -20,13 +20,36 @@ const load = (config, messages) => {
     messages.forEach((msg, index) => {
       simpleParser(msg, parseOptions)
         .then((parsed) => {
-          //console.info(parsed);
+
           const sql = `
-          SELECT SESSION_USER, CURRENT_SCHEMA 
-          FROM "DUMMY"
+          INSERT INTO "MBOX_SCHEMA"."EMAIL" (
+            EMAIL_DATE,
+            MESSAGE_ID,
+            FROM_ADDRESS,
+            TO_ADDRESS,
+            CC_ADDRESS,
+            BCC_ADDRESS,
+            SUBJECT,
+            TEXT,
+            TEXT_AS_HTML
+          )
+          VALUES (TO_DATE(?, 'YYYY-MM-DD'),?,?,?,?,?,?,?,?);
           `;
 
-          client.exec(sql, (err, result) => {
+          const stringDate = `${parsed.date.getFullYear()}-${parsed.date.getMonth() + 1}-${parsed.date.getDate()}`
+          
+          const bindParams = [
+            stringDate,
+            parsed.messageId,
+            parsed.from ? parsed.from.text : null,
+            parsed.to ? parsed.from.text : null,
+            parsed.cc ? parsed.cc.text : null,
+            parsed.bcc ? parsed.bcc.text : null,
+            parsed.subject,
+            parsed.text,
+            parsed.textAsHtml
+          ];
+          client.exec(sql, bindParams, (err, result) => {
             if (err) {
               console.error('SQL error', err);
               throw err;
